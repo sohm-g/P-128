7d5a00e59dbc55c85c76074137001b95ee9807d4
sohm-g <77761242+sohm-g@users.noreply.github.com>
2021-12-08 17:16:13 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+song1 = "";
+song2 = "";
+
+song1_status = "";
+song2_status = "";
+
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function preload()
+{
+	song1 = loadSound("s1.mp3");
+	song2 = loadSound("s2.mp3");
+}
+
+function setup() {
+	canvas =  createCanvas(600, 500);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
